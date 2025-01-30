068141d6aafc8db356e32348351a03cafbb27ff9
Andres Correa Sanchez <andres-co-vg@hotmail.es>
2025-01-30 21:37:59 +0100

    Agregar script de análisis y limpieza de datos, y generar conjunto de datos limpio

--- src/analisis_inicial.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from ydata_profiling import ProfileReport
-
-df = pd.read_csv("../data/data_set_ecommerce.csv", encoding='ISO-8859-1')
-
-# Generar reporte de para ahcer la exploración inicial de los datos
-# profile = ProfileReport(df, title = "E-Commerce DataSet Profiling Report", explorative = True)
-# profile.to_file("../reports/data_set_ecommerce.html", False)
---
+++ src/analisis_inicial_y_limpieza.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+from ydata_profiling import ProfileReport
+
+df = pd.read_csv("../data/data_set_ecommerce.csv", encoding='ISO-8859-1')
+
+# Generar reporte de para ahcer la exploración inicial de los datos
+# profile = ProfileReport(df, title = "E-Commerce DataSet Profiling Report", explorative = True)
+# profile.to_file("../reports/data_set_ecommerce.html", False)
+
+# Eliminar registros con valores negativos en el campo Quantity cuyo campo invoceNo no empiece con 'C'
+df = df[~((df['Quantity'] < 0) & (~df['InvoiceNo'].str.startswith('C')))]
+
+# Rellenar campos nulos o vacíos en la columna Description
+df['Description'] = df['Description'].fillna('No Description')
+
+# Cambiar valores negativos de UnitPrice a positivos y asegurar que todos sean números reales con decimales
+df['UnitPrice'] = df['UnitPrice'].abs().astype(float)
+
+# Rellenar CustomerID vacíos con un número aleatorio de 5 dígitos
+df['CustomerID'] = df['CustomerID'].apply(lambda x: np.random.randint(10000, 99999) if pd.isnull(x) else x)
+
+# Guardar el dataset limpio en un nuevo archivo
+df.to_csv("../data/data_set_clean.csv", index = False)
