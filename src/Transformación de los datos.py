dcf8945721a8e132b73b5a9a50bf13c74132211d
flopqui <fjlq.computer@gmail.com>
2025-02-07 17:17:09 +0100

    Transformación de los datos.py

--- src/Transformación de los datos.py
+++ src/Transformación de los datos.py
@@ -32,18 +32,44 @@ InvoiceNo	StockCode	Description	Quantity	InvoiceDate	UnitPrice	CustomerID	Countr
 # Calculamos las ventas totales por cada transacción
 df['TotalSales'] = df['Quantity'] * df['UnitPrice']
 
+# Crear una nueva columna 'TotalSales' como el valor de la venta en cada transacción
+# Suponemos que la columna 'Price' corresponde al precio de cada transacción y 'Quantity' la cantidad vendida
+df['TotalSales'] = df['UnitPrice'] * df['Quantity']
 
-# Convertimos 'InvoiceDate' a formato datetime
-df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
+# Revisamos los primeros registros
+df.head(10)
+
+
+InvoiceNo	StockCode	Description	Quantity	InvoiceDate	UnitPrice	CustomerID	Country	TotalSales
+0	536365	85123A	WHITE HANGING HEART T-LIGHT HOLDER	6	2010-12-01 08:26:00	2.55	17850.0	United Kingdom	15.30
+1	536365	71053	WHITE METAL LANTERN	6	2010-12-01 08:26:00	3.39	17850.0	United Kingdom	20.34
+2	536365	84406B	CREAM CUPID HEARTS COAT HANGER	8	2010-12-01 08:26:00	2.75	17850.0	United Kingdom	22.00
+3	536365	84029G	KNITTED UNION FLAG HOT WATER BOTTLE	6	2010-12-01 08:26:00	3.39	17850.0	United Kingdom	20.34
+4	536365	84029E	RED WOOLLY HOTTIE WHITE HEART.	6	2010-12-01 08:26:00	3.39	17850.0	United Kingdom	20.34
+5	536365	22752	SET 7 BABUSHKA NESTING BOXES	2	2010-12-01 08:26:00	7.65	17850.0	United Kingdom	15.30
+6	536365	21730	GLASS STAR FROSTED T-LIGHT HOLDER	6	2010-12-01 08:26:00	4.25	17850.0	United Kingdom	25.50
+7	536366	22633	HAND WARMER UNION JACK	6	2010-12-01 08:28:00	1.85	17850.0	United Kingdom	11.10
+8	536366	22632	HAND WARMER RED POLKA DOT	6	2010-12-01 08:28:00	1.85	17850.0	United Kingdom	11.10
+9	536367	84879	ASSORTED COLOUR BIRD ORNAMENT	32	2010-12-01 08:34:00	1.69	13047.0	United Kingdom	54.08
+
+# Dividir el conjunto de datos en conjuntos de entrenamiento y prueba
+fecha_inicio_entrenamiento = '2010-12-01'
+fecha_fin_entrenamiento = '2011-11-08'
+fecha_inicio_prueba = '2011-11-09'
+fecha_fin_prueba = '2011-12-09'
+
+# Filtrar los datos de entrenamiento y prueba
+train_data = df_daily_sales[(df_daily_sales['InvoiceDate'] >= fecha_inicio_entrenamiento) & (df_daily_sales['InvoiceDate'] <= fecha_fin_entrenamiento)]
+test_data = df_daily_sales[(df_daily_sales['InvoiceDate'] >= fecha_inicio_prueba) & (df_daily_sales['InvoiceDate'] <= fecha_fin_prueba)]
+
+         
+PREPARACIÓN DE LAS CARACTERÍSTICAS Y LA VARIABLE OBJETO
 
 # Creamos columnas para el año, mes, día, día de la semana y semana del año
 df['Year'] = df['InvoiceDate'].dt.year
 df['Month'] = df['InvoiceDate'].dt.month
 df['Day'] = df['InvoiceDate'].dt.day
 df['DayOfWeek'] = df['InvoiceDate'].dt.dayofweek
-
-
-# Revisamos los primeros registros
 df.head(10)
 
 InvoiceNo	StockCode	Description	Quantity	InvoiceDate	UnitPrice	CustomerID	Country	TotalSales	Year	Month	Day	DayOfWeek
@@ -58,52 +84,30 @@ InvoiceNo	StockCode	Description	Quantity	InvoiceDate	UnitPrice	CustomerID	Countr
 8	536366	22632	HAND WARMER RED POLKA DOT	6	2010-12-01 08:28:00	1.85	17850.0	United Kingdom	11.10	2010	12	1	2
 9	536367	84879	ASSORTED COLOUR BIRD ORNAMENT	32	2010-12-01 08:34:00	1.69	13047.0	United Kingdom	54.08	2010	12	1	2
 
+         
+# Selección de características (X) y variable objetivo (y)
+X_train = df[['DayOfWeek', 'Day', 'Month', 'Year']]
+y_train = df['TotalSales']
+X_test = df[['DayOfWeek', 'Day', 'Month', 'Year']]
+y_test = df['TotalSales']
 
 
-# Preparamos las variables con los datos que vamos a utilizar
-X = df[['Year', 'Month', 'Day', 'DayOfWeek', 'Quantity', 'UnitPrice']] # Variables de entrada
-y = df['TotalSales'] # Variable de salida
-
-
- # Dividimos los datos en entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-print("Datos divididos en entrenamiento y prueba")
-
-Datos divididos en entrenamiento y prueba
-
-CREACIÓN Y ENTRENAMIENTO DEL MODELO
-
- model = RandomForestRegressor(n_estimators=100, random_state=42) # Inicializar y entrenar el modelo de RandomForestRegressor
-model.fit(X_train, y_train) # Entrenamos el modelo
-
-y_pred = model.predict(X_test) # Realizar predicciones sobre el conjunto de prueba
-
-print("Modelo entrenado")
-Modelo entrenado
+ENTRENAMIENTO DEL MODELO
 
 # Graficar las predicciones vs los valores reales
 plt.figure(figsize=(10, 6))
-plt.plot(test_data['fecha'], y_test, label='Valores reales', color='blue')
-plt.plot(test_data['fecha'], y_pred, label='Predicciones', color='red', linestyle='--')
+plt.plot(df['InvoiceDate'], y_test, label='Valores reales', color='blue')
+plt.plot(df['InvoiceDate'], y_pred, label='Predicciones', color='red', linestyle='--')
 plt.title('Predicción de ventas diarias')
 plt.xlabel('Fecha')
-plt.ylabel('Total de ventas')
+plt.ylabel('Ventas Totales')
 plt.legend()
 plt.show()
 
-      
- 
 
 
 
 
 
-
-
-
-
-
-
-
-
+         
 
